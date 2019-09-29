ad103d2bc9a7f9c464d41e6beb5c55d2f310586e
emilioheinz <emiliosheinz@gmail.com>
2019-09-29 20:23:13 -0300

    Connecting sagas and reducers

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
+import { exampleRequest } from '~/redux/modules/example/actions'
 // import { Container } from './styles';
 
 export default function Home() {
-  return <h1>HOME</h1>
+  const dispatch = useDispatch()
+
+  function buttonPressed() {
+    const requestParams = {
+      firstNumber: 3,
+      secondNumber: 4,
+    }
+
+    dispatch(exampleRequest(requestParams))
+  }
+
+  return (
+    <>
+      <h1>HOME</h1>
+      <button type='button' onClick={buttonPressed}>
+        CLICK HERE TO TEST
+      </button>
+    </>
+  )
 }
--- src/redux/modules/example/reducer.js
+++ src/redux/modules/example/reducer.js
@@ -1,7 +1,14 @@
-const INITIAL_STATE = {}
+const INITIAL_STATE = {
+  exampleResult: null,
+}
 
 export default function example(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case '@example/EXAMPLE_SUCCESS':
+      return {
+        ...state,
+        exampleResult: action.payload.result,
+      }
     default:
       return state
   }
